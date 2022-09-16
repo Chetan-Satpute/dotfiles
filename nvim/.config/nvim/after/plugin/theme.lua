c13eb576db5ec048c719bd6cb6ad8d13cbd77171
Chetan Satpute <chetan.satpute2002@gmail.com>
2022-09-16 13:27:13 +0530

    Changed color theme

--- nvim/.config/nvim/after/plugin/theme.lua
+++ nvim/.config/nvim/after/plugin/theme.lua
@@ -1,3 +1,4 @@
+--[[
 require("tokyonight").setup({
 	-- your configuration comes here
 	-- or leave it empty to use the default settings
@@ -37,6 +38,34 @@ require("tokyonight").setup({
 
 vim.g.tokyonight_style = "night"
 vim.opt.background = "dark"
+]]
 
-vim.cmd([[colorscheme tokyonight]])
+vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
 
+require("catppuccin").setup({
+	integrations = {
+		cmp = true,
+		gitsigns = true,
+		telescope = true,
+		treesitter = true,
+		treesitter_context = false,
+		ts_rainbow = false,
+		native_lsp = {
+			enabled = true,
+			virtual_text = {
+				errors = { "italic" },
+				hints = { "italic" },
+				warnings = { "italic" },
+				information = { "italic" },
+			},
+			underlines = {
+				errors = { "underline" },
+				hints = { "underline" },
+				warnings = { "underline" },
+				information = { "underline" },
+			},
+		},
+	},
+})
+
+vim.cmd([[colorscheme catppuccin]])
